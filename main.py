170fa926ec0764e9cdf8040a9b6ce22793a91860
Zlebub <tayloamb015@mead354.org>
2024-05-28 14:16:33 -0700

    Add my program

--- main.py
+++ main.py
@@ -6,9 +6,29 @@ students = ['abigail','ben','christi','dave']
 
 print(first)
 # what happens when we add two lists?
-print(evens + students)'''
+print(evens + students)
 # Fancy things I can do with a list
 evens = [2*x for x in range(1,6)]
 
 for e in evens:
     print(str(e) + ' is 2 * ' + str(int(e/2)))
+# My programs
+# Adding to the list
+team = []
+
+player = ''
+while player != 'done':
+    player = input('give me a player name: ')
+    if player != 'done':
+        team.append(player)
+team.sort()
+
+print('your team is: ')
+for p in team:
+    print("\t" + p)
+
+if 'Xavier' in team:
+    print("I can't wait to play")
+else:
+    print("Put me in coach, I'm ready to play.")'''
+# Grocery

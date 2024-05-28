47fd7b3cb640b862c3121714a5202802a8ec1b96
Zlebub <tayloamb015@mead354.org>
2024-05-28 13:46:01 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Examples
+first = ['this','is','a','list']
+evens = [2,4,6,8,10]
+students = ['abigail','ben','christi','dave']
+
+print(first)
+# what happens when we add two lists?
+print(evens + students)

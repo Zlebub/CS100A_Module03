90953a8c6a76a35bd9e45f280c5ee07c898ca1f6
Zlebub <tayloamb015@mead354.org>
2024-05-28 14:11:52 -0700

    Adding examples

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Examples
-first = ['this','is','a','list']
+# Adding lists
+'''first = ['this','is','a','list']
 evens = [2,4,6,8,10]
 students = ['abigail','ben','christi','dave']
 
 print(first)
 # what happens when we add two lists?
-print(evens + students)
+print(evens + students)'''
+# Fancy things I can do with a list
+evens = [2*x for x in range(1,6)]
+
+for e in evens:
+    print(str(e) + ' is 2 * ' + str(int(e/2)))
